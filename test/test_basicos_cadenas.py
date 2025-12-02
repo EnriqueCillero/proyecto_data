02e087d020962beef97eb63996e9deb602af709c
Enrique Cillero Dorado <encd@gmv.com>
2025-12-02 19:29:15 +0100

    count_words test added

--- test/test_basicos_cadenas.py
+++ test/test_basicos_cadenas.py
@@ -10,3 +10,18 @@ def test_normalize_str():
     assert normalize_str("  HoLA  ") == "hola"
     assert normalize_str("MUNDO") == "mundo"
     assert normalize_str("\t Espacio \n") == "espacio"
+
+
+def test_count_words():
+    text = "Hola hola, HOLA!! adiós?"
+    expected = {"hola": 3, "adiós": 1}
+    assert count_words(text) == expected
+
+    assert count_words("uno, dos; tres tres tres.") == {
+        "uno": 1,
+        "dos": 1,
+        "tres": 3
+    }
+
+    # word with only symbols ignored
+    assert count_words("!!! ??? ...") == {}
