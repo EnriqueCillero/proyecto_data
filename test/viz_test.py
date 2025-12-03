6b84eac5995a112acfa5f56c8492ebf649104742
Enrique Cillero Dorado <encd@gmv.com>
2025-12-03 21:27:11 +0100

    Absolute path added to test

--- test/viz_test.py
+++ test/viz_test.py
@@ -2,11 +2,18 @@ import os
 from proyecto import plot_line_time_series, plot_bar, plot_scatter
 
 
+
+# Guardamos el path base que queremos utilizar y creamos la carpeta
+BASE_PATH = os.Path(__file__).resolve().parent.parent
+FIGURES = BASE_PATH / "figures"
+FIGURES.mkdir(exist_ok=True)
+
+
 # --------------------------
 # TEST plot_line_time_series
 # --------------------------
 def test_plot_line_time_series():
-    out_path = "figures/test_line.png"
+    out_path =  FIGURES / "test_line.png"
 
     xs = [0, 1, 2, 3]
     ys = [10, 15, 8, 20]
@@ -22,7 +29,7 @@ def test_plot_line_time_series():
 # TEST plot_bar
 # --------------------------
 def test_plot_bar():
-    out_path = "figures/test_bar.png"
+    out_path = FIGURES/ "test_bar.png"
 
     categories = ["A", "B", "C"]
     values = [5, 10, 7]
@@ -38,7 +45,7 @@ def test_plot_bar():
 # TEST plot_scatter
 # --------------------------
 def test_plot_scatter():
-    out_path = "figures/test_scatter.png"
+    out_path = FIGURES / "test_scatter.png"
 
     x = [1, 2, 3, 4]
     y = [2, 4, 6, 8]
